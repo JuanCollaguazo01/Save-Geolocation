20b397bdd2b4577e0af7dfcfc83e7e1fe0592434
Juan Collaguazo <juan.collaguazo01@epn.edu.ec>
2021-07-20 11:43:59 -0500

    golocalizacion

---
+++ geolocation/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+  apiKey: "AIzaSyDZatFVi5JFHOSmXk53fBbJEppLrctfQEs",
+  authDomain: "crud2-41c4b.firebaseapp.com",
+  projectId: "crud2-41c4b",
+  storageBucket: "crud2-41c4b.appspot.com",
+  messagingSenderId: "55290192281",
+  appId: "1:55290192281:web:120bf93667840426b37308",
+  measurementId: "G-VSVLJ85DWH"
+}
+};
---
+++ geolocation/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+  apiKey: "AIzaSyDZatFVi5JFHOSmXk53fBbJEppLrctfQEs",
+  authDomain: "crud2-41c4b.firebaseapp.com",
+  projectId: "crud2-41c4b",
+  storageBucket: "crud2-41c4b.appspot.com",
+  messagingSenderId: "55290192281",
+  appId: "1:55290192281:web:120bf93667840426b37308",
+  measurementId: "G-VSVLJ85DWH"
+}
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
